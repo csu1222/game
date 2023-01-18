f8a9698214f7524303cb6a1fc5d1649d1fb81609
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-01-18 17:07:18 +0900

    DFS - 깊이 우선 탐색 Depth First Search,

--- Part_3_DataStructure_and_Algorithm/Algorithm/Algorithm/Algorithm.cpp
+++ Part_3_DataStructure_and_Algorithm/Algorithm/Algorithm/Algorithm.cpp
@@ -5,152 +5,91 @@
 #include <queue>
 using namespace std;
 
-void CreateGraph_1()
+// DFS (Depth First Search) 깊이 우선 탐색
+// BFS (Breadth First Search) 너비 우선 탐색
+
+struct Vertex
 {
-	struct Vertex
-	{
-		vector<Vertex*> edges;
-	};
-
-	vector<Vertex> v;
-	v.resize(6); 
-
-	v[0].edges.push_back(&v[1]);
-	v[0].edges.push_back(&v[3]);
-	v[1].edges.push_back(&v[0]);
-	v[1].edges.push_back(&v[2]);
-	v[1].edges.push_back(&v[3]);
-	v[3].edges.push_back(&v[4]);
-	v[5].edges.push_back(&v[4]);
-
-	// Q. 0번 -> 3번 정점이 연결되어 있나요?
-	bool connected = false;
-	for (Vertex* edge : v[0].edges)
-	{
-		if (edge == &v[3])
-		{
-			connected = true;
-			break;
-		}
-	}
+	// int _data;
 };
 
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+vector<bool> visited;
 
-void CreateGraph_2()
+void CreateGraph ()
 {
-	struct Vertex
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6);
+	// 내용을 넣어주는 방법으로 push_back 이나 {} 초기화를 사용합니다.
+
+	//// 인접 리스트
+	//adjacent[0].push_back(1);
+	//adjacent[0].push_back(3);
+	//adjacent[1].push_back(0);
+	//adjacent[1].push_back(2);
+	//adjacent[1].push_back(3);
+	//adjacent[3].push_back(4);
+	//adjacent[5].push_back(4);
+
+	// 인접 행렬
+	adjacent = vector<vector<int>>
 	{
-		// int _data;
+		{ 0, 1, 0, 1, 0, 0},
+		{ 1, 0, 1, 1, 0, 0},
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 1, 0},
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 1, 0},
 	};
 
-	vector<Vertex> v;
-	v.resize(6);
+}
 
-
-	vector<vector<int>> adjacent(6);
-
-	adjacent[0] = { 1, 3 };
-	adjacent[1] = { 0, 2, 3 };
-	adjacent[3] = { 4 };
-	adjacent[5] = { 4 };
-
-	// Q. 0번 -> 3번 정점이 연결되어 있나요?
-	bool connected = false;
-	for (int vtx : adjacent[0])
-	{
-		if (vtx == 3)
-		{
-			connected = true;
-			break;
-		}
-	}
-	// STL
-	vector<int> adj = adjacent[0];
-
-	bool connected_2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
-
-	// 가중치 표현 
-	pair<int, int> ;
-
-	vector<vector<pair<int, int>>> adjacent2(6);
-
-	adjacent2[0] = { { 1,15 } , { 3, 35 } };
-	adjacent2[1] = { { 0, 15 } , { 2, 5 } , { 3 , 10 } };
-	adjacent2[3] = { { 4, 5 } };
-	adjacent2[5] = { { 4, 5 } };
-
-	// Q. 0번 -> 3번 정점이 연결되어 있나요?
-	bool WeightConnected = false;
-	for (pair<int, int> vtx : adjacent2[0])
-	{
-		if (vtx.first == 3)
-		{
-			connected = true;
-			break;
-		}
-	}
-	// STL
-	vector<pair<int, int>> WeightAdj = adjacent2[0];
-
-	bool WeightConnected_2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
-
-};
-
-void CreateGraph_3()
+void Dfs(int here)
 {
-	struct Vertex
+	// 방문!
+	visited[here] = true;
+
+	cout << "Visited : " << here << endl;
+	
+	// 인접 리스트 버전
+	// 모든 인접 정점을 순회한다 
+	//for (int i = 0; i < adjacent[here].size(); i++)
+	//{
+	//	// 방문할 방 there
+	//	int there = adjacent[here][i];
+
+	//	// 방문했는지 체크 후 방문하지 않았으면 방문 
+	//	if (visited[there] == false)
+	//		Dfs(there);
+	//}
+
+	// 인접 행렬 버전
+	for (int there = 0; there < 6; there++)
 	{
-		// int _data;
-	};
+		if (adjacent[here][there] == 0)
+			continue;
 
-	vector<Vertex> v;
-	v.resize(6);
+		if (visited[there] == false)
+			Dfs(there);
+	}
+}
 
-	// 연결된 목록을 행렬로 관리 
-	//	[X][O][X][O][X][X]
-	//	[O][X][O][O][X][X]
-	//	[X][X][X][X][X][X]
-	//	[X][X][X][X][O][X]
-	//	[X][X][X][X][X][X]
-	//	[X][X][X][X][O][X]
+void DfsAll()
+{
 
-	// 읽는 방법 : adjacent[form][to]
-	// 행렬을 이용한 그래프 표현(2차원 배열)
-	// 메모리 소모가 심하지만 빠른 접근이 가능
-	// (간선이 많은 경우 이점이 있다.)
-	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+	visited = vector<bool>(6, false);
 
-	adjacent[0][1] = true;
-	adjacent[0][3] = true;
-	adjacent[1][0] = true;
-	adjacent[1][2] = true;
-	adjacent[1][3] = true;
-	adjacent[3][4] = true;
-	adjacent[5][4] = true;
-
-	// Q. 0번 -> 3번 정점이 연결되어 있나요?
-	bool connected = adjacent[0][3];
-
-	// 가중치 표현 
-	vector<vector<int>> adjacent2 = 
-	{
-		vector<int> { -1, 15, -1, 35, -1, -1},
-		vector<int> { 15, -1, +5, 10, -1, -1},
-		vector<int> { -1, -1, -1, -1, -1, -1},
-		vector<int> { -1, -1, -1, -1, +5, -1},
-		vector<int> { -1, -1, -1, -1, -1, -1},
-		vector<int> { -1, -1, -1, -1, +5, -1},
-	};
-
-	// Q. 0번 -> 3번 정점이 연결되어 있나요?
-	bool connected2 = adjacent2[0][3] > -1;
-
-	// Q2. 0번 -> 4번의 가중치는 얼마인가요?
-	int weight = adjacent2[0][4];
-};
+	for (int i = 0; i < 6; i++)
+		if (visited[i] == false)
+			Dfs(i);
+}
 
 int main()
 {
-	CreateGraph_1();
-	CreateGraph_2();
+	CreateGraph();
+
+	// visited = vector<bool>(6, false);
+
+	DfsAll();
 }
