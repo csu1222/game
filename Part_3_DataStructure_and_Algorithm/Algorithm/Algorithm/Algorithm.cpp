85ff15e6d20bda3c805c2a03913d680d89c8c554
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-02-08 16:01:29 +0900

    퀵 정렬 - 피벗, low, high 를 사용하는 퀵 정렬

--- Part_3_DataStructure_and_Algorithm/Algorithm/Algorithm/Algorithm.cpp
+++ Part_3_DataStructure_and_Algorithm/Algorithm/Algorithm/Algorithm.cpp
@@ -6,166 +6,68 @@
 using namespace std;
 #include <thread>
 
-// 오늘의 주제 : 힙 정렬, 병합 정렬
+// 오늘의 주제 : 퀵 정렬 Quick Sort
 
-void HeapSort(vector<int>& v)
+
+// [5][1][3][7][9][2][4][6][8]
+//  p
+//    low				   high
+int Partition(vector<int>& v, int left, int right)
 {
-	priority_queue<int, vector<int>, greater<int>> pq;
+	// 피벗 , 로우, 하이 
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
 
-	for (int num : v)
-		pq.push(num);
-
-	v.clear();
-
-	while (pq.empty() == false)
+	// low 와 high 가 교차되는 때 까지 반복 
+	while (low <= high)
 	{
-		v.push_back(pq.top());
-		pq.pop();
+		// 1 단계
+		// - pivot >= arr[low] 일 동안 low 를 오른쪽으로 이동
+		while (low <= right && pivot >= v[low])
+			low++;
+
+		// - pivot <= arr[high] 일 동안 high 를 왼쪽으로 이동
+		while (high >= left + 1 && pivot <= v[high])
+			high--;
+
+		// 2 단계
+		// - low < high 라면 arr[low]와 arr[high] 데이터 교체
+		if (low < high)
+			swap(v[low], v[high]);
 	}
+
+	// 3 단계
+	// - high <= low 면 빠져나오고, pivot과 arr[high] 교체
+	swap(v[left], v[high]);
+
+	return high;
 }
 
-void MergeResult(vector<int>& v, int left, int mid, int right)
+void QuickSort(vector<int>& v, int left, int right)
 {
-	int leftIdx = left;
-	int rightIdx = mid + 1;
-
-	vector<int> temp;
-
-	while (leftIdx <= mid && rightIdx <= right)
-	{
-		if (v[leftIdx] <= v[rightIdx])
-		{
-			temp.push_back(v[leftIdx]);
-			leftIdx++;
-		}
-		else
-		{
-			temp.push_back(v[rightIdx]);
-			rightIdx++;
-		}
-	}
-	
-	// 위의 while 문을 빠져 나왔다고 끝난게 아니라 두 덩어리중 한 덩어리가 먼저 끝났다는 이야기
-
-	// 남은 데이터들을 다 push 합니다. 
-
-	// 왼쪽이 먼저 끝났으면, 오른쪽의 나머지 데이터 복사
-	if (leftIdx > mid)
-	{
-		while (rightIdx <= right)
-		{
-			temp.push_back(v[rightIdx]);
-			rightIdx++;
-		}
-	}
-	// 오른쪽이 먼저 끝났으면, 왼쪽의 나머지 데이터 복사
-	else
-	{
-		while (leftIdx <= mid)
-		{
-			temp.push_back(v[leftIdx]);
-			leftIdx++;
-		}
-	}
-
-	// v 에 temp 를 하나씩 복사합니다. 그런데 v의 모든 영역을 항상 사용할 수 있는건아니고 
-	// 인자로 받은 left 의 위치 부터 복사해나가야 하는데 그 이유는 
-	// 현재 재귀 함수로 영역을 찝어서 진행하고 있기 때문입니다. 
-	for (int i = 0; i < temp.size(); i++)
-		v[left + i] = temp[i];
-
-}
-
-void MergeSort(vector<int>& v, int left, int right)
-{
-	if (left >= right)
+	if (left > right)
 		return;
+	
+	int pivot = Partition(v, left, right);
 
-	int mid = (left + right) / 2;
-
-	MergeSort(v, left, mid);
-	MergeSort(v, mid + 1, right);
-
-	MergeResult(v, left, mid, right);
-}
-
-vector<int> Merge(vector<int>& a, vector<int>& b)
-{
-	int aIdx = 0;
-	int bIdx = 0;
-
-	vector<int> tem2;
-
-	while (aIdx <= a.size() - 1 && bIdx <= b.size() - 1)
-	{
-		if (a[aIdx] <= b[bIdx])
-		{
-			tem2.push_back(a[aIdx]);
-			aIdx++;
-		}
-		else
-		{
-			tem2.push_back(b[bIdx]);
-			bIdx++;
-		}
-
-	}
-
-	if (aIdx < a.size() - 1)
-	{
-		while (aIdx <= a.size() - 1)
-		{
-			tem2.push_back(a[aIdx]);
-			aIdx++;
-		}
-	}
-	else
-	{
-		while (bIdx <= b.size() - 1)
-		{
-			tem2.push_back(b[bIdx]);
-			bIdx++;
-		}
-	}
-	vector<int> tem = {};
-	for (int i = 0; i < tem2.size(); i++)
-	{
-		tem.push_back(tem2[i]);
-	}
-
-	return tem;
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
 }
 
 int main()
 {
 	vector<int> v;
-	
-	srand(time(0));
+
+	srand(static_cast<int>(time(0)));
+
 
 	for (int i = 0; i < 50; i++)
 	{
 		int randValue = rand() % 100;
+
 		v.push_back(randValue);
 	}
 
-	// HeapSort(v);
-	MergeSort(v, 0, v.size() - 1);
-
-
-	vector<int> a;
-	vector<int> b;
-
-	for (int i = 0; i < 25; i++)
-	{
-		int aValue = rand() % 25;
-		int bValue = rand() % 25;
-		a.push_back(aValue);
-		b.push_back(bValue);
-	} 
-
-	MergeSort(a, 0, a.size()-1);
-	MergeSort(b, 0, b.size()-1);
-
-
-	vector<int> result = Merge(a, b);
+	QuickSort(v, 0, v.size() - 1);
 }
