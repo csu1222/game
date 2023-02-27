677dbc6651840139a906f442b3067432a133deef
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-02-27 15:36:20 +0900

    데드락 - 락사이의 교착상태, 락의 순서를 맞춰줌으로 대응가능

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -5,62 +5,43 @@
 #include <atomic>
 #include <mutex>
 
-// Lock 기초 
+#include "AccountManager.h"
+#include "UserManager.h"
 
-vector<int32> v;
-
-// Mutual Exclusive (상호배타적)
-mutex m;
-
-// RAII(Resource Acquisition Is Initialization)
-template<typename T>
-class LockGuard
+void Func1()
 {
-public:
-	LockGuard(T& m)
+	for (int32 i = 0; i < 100; i++)
 	{
-		_mutex = &m;
-		_mutex->lock();
-	}
-	~LockGuard()
-	{
-		_mutex->unlock();
-	}
-
-private:
-	T* _mutex;
-};
-
-void Push()
-{
-	for (int32 i = 0; i < 10000; i++)
-	{
-		// 자물쇠 잠그기
-		//std::lock_guard<std::mutex> lockGuard(m);
-		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
-		uniqueLock.lock();
-		//m.lock();
-
-		v.push_back(i);
-
-		if (i == 5000)
-		{
-			// m.unlock();
-			break;
-		}
-
-		// 자물쇠 풀기
-		//m.unlock();
+		UserManager::Instance()->ProcessSave();
 	}
 }
 
+void Func2()
+{
+	for (int32 i = 0; i < 100; i++)
+	{
+		AccountManager::Instance()->ProcessLogin();
+	}
+}
+
+
 int main()
 {
-	std::thread t1(Push);
-	std::thread t2(Push);
+	std::thread t1(Func1);
+	std::thread t2(Func2);
 
 	t1.join();
 	t2.join();
 
-	cout << v.size() << endl;
+	cout << "Jobs Done" << endl;
+
+
+	// 참고 
+	mutex m1;
+	mutex m2;
+	std::lock(m1, m2);	// m1.lock(); m2.lock();
+
+	// adopt_lock : 이미 lock된 상태니까, 나중에 소멸될 때 풀어주기만 해
+	lock_guard<mutex> g1(m1, std::adopt_lock);
+	lock_guard<mutex> g2(m2, std::adopt_lock);
 }
