13733bb331f6a61e9e036cb15a94957d0b54657f
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-03-02 16:22:05 +0900

    스핀 락 구현 - 첫번째 락 구현 atomic 으로 bool 타입을 관리하고 compare_exchange_strong 메소드로 스핀락 뺑뺑이를 구현

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -5,43 +5,77 @@
 #include <atomic>
 #include <mutex>
 
-#include "AccountManager.h"
-#include "UserManager.h"
-
-void Func1()
+class SpinLock
 {
-	for (int32 i = 0; i < 100; i++)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		// CAS (Compare-And_Swap)
+
+		bool expected = false;
+		bool desired = true;
+
+		// CAS 의사 코드 
+		/*if (_locked == expected)
+		{
+			expected = _locked;
+			_locked = desired;
+			return true;
+		}
+		else
+		{
+			expected = _locked;
+			return false;
+		}*/
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+
+	}
+
+	void unlock()
+	{
+		//_locked = false;
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add()
+{
+	for (int i = 0; i < 100000; i++)
+	{
+		lock_guard<SpinLock> gaurd(spinLock);
+		sum++;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int32 i = 0; i < 100; i++)
+	for (int i = 0; i < 100000; i++)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> gaurd(spinLock);
+		sum--;
 	}
 }
-
 
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+
+
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << "Jobs Done" << endl;
-
-
-	// 참고 
-	mutex m1;
-	mutex m2;
-	std::lock(m1, m2);	// m1.lock(); m2.lock();
-
-	// adopt_lock : 이미 lock된 상태니까, 나중에 소멸될 때 풀어주기만 해
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g2(m2, std::adopt_lock);
+	cout << sum << endl;
 }
