687b650b707bc36bc9b3fdac4b0569712ebe6b93
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-03-07 17:14:19 +0900

    Condition Variable 조건 변수 - Event와 비슷하지만 레벨 영역에서 동작하는 개념

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -10,18 +10,26 @@ mutex  m;
 queue<int32> q;
 HANDLE handle;
 
+// 참고) CV 는 User-Level Object (커널 오브젝트X)
+condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고 
+		// 4) 조건변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
+		cv.notify_one(); // wait 중인 쓰레드가 있으면 딱 1개를 깨운다
 
-		this_thread::sleep_for(1000ms);
+		this_thread::yield();
 	}
 }
 
@@ -29,15 +37,22 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
 
 		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() { return q.empty() == false; });
+		// 1) Lock을 잡고 
+		// 2) 조건 확인
+		// - 만족 O -> 빠져 나와서 이어서 코드를 진행 
+		// - 만족 X -> Lock을 풀어주고 대기 상태로 전환 
 
-		if (q.empty() == false)
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까>? 
+		// Spurious Wakeup (직영하면 가짜 기상?)
+		// notify_one을 할 때 lock을 잡고 있는 것이 아니기 때문
+
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
@@ -48,13 +63,10 @@ int main()
 	// Usage Count		몇개나 참조하고 있는가
 	// Signal (파란불) / Non-Signal (빨간불)  << bool 
 	// Auto / Manual  << bool 
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bIntialState*/, NULL/*Name*/);
-
 	thread t1(Producer);
 	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	::CloseHandle(handle);
 }
