38a809da1124b36ac55a56d01951e3ea8e2112ba
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-03-13 17:54:34 +0900

    컴퓨터 구조 Cache - 캐시의 구성 원리 두가지와 그에 따른 성능 차이 실습

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -7,96 +7,38 @@
 #include <Windows.h>
 #include <future>
 
-int64 Calculator()
-{
-	int32 sum = 0;
+int32 buffer[10000][10000];
 
-	for (int32 i = 0; i < 100'000; i++)
-		sum += i;
-
-	return sum;
-}
-
-void PromiseWorker(std::promise<string>&& promise)
-{
-	promise.set_value("Secret Message");
-}
-
-void TaskWorker(std::packaged_task < int64(void)> && task)
-{
-	task();
-}
 
 int main()
 {
-	// 동기(Synchronus) 실행
-	// int64 sum = Calculator();
+	memset(buffer, 0, sizeof(buffer));
 
-
-	// std::future
 	{
-		std::future<int64> future = std::async(std::launch::async, Calculator);
+		uint64 start = GetTickCount64();
 
-		// TODO 
-		// future 객체의 작업이 끝났는지 알아보기 
-		std::future_status status = future.wait_for(1ms);
+		int64 sum = 0;
 
-		if (status == future_status::ready)
-		{
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[i][j];
 
-		}
+		uint64 end = GetTickCount64();
 
-		int64 sum = future.get();
-
+		cout << "Elapsed Tick " << (end - start) << endl;
 	}
 
-		// 추가 디테일 : future가 들고 있을 함수가 다른 객체의 메소드라면?
 	{
-		class Knight
-		{
-		public:
-			int64 GetHP() { return 100; }
-		};
+		uint64 start = GetTickCount64();
 
-		Knight knight;
-		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHP, knight);
+		int64 sum = 0;
+
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[j][i];
+
+		uint64 end = GetTickCount64();
+
+		cout << "Elapsed Tick " << (end - start) << endl;
 	}
-
-	// std::promise
-	{
-		// 미래(std::futur)에 결과물을 반환해줄꺼라 약속(std::promise) 해줘 (계약서?)
-		std::promise<string> promise;
-		std::future<string> future = promise.get_future();
-
-		thread t(PromiseWorker, std::move(promise));
-
-		string message = future.get();
-		cout << message << endl;
-
-		t.join();
-	}
-
-	// packaged_task
-	{
-		std::packaged_task<int64(void)> task(Calculator);
-		std::future<int64> future = task.get_future();
-
-		std::thread t(TaskWorker, std::move(task));
-
-		int64 sum = future.get();
-		cout << sum << endl;
-
-		t.join();
-	}
-
-	// 결론)
-	// mutex, condition_variable 까지 가지 않고 단순한 애들을 처리할 수 있는 방법
-	// 특히나, 한번 발생하는 이벤트에 유용하다!
-	// 닭잡는데 소잡는 칼을 쓸 필요 없다!
-	// 1) async
-	// 원하는 함수를 비동기적으로 실행
-	// 2) promise
-	// 결과물을 promise를 통해 future로 받아줌 
-	// 3) packaged_task
-	// 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌 
 }
