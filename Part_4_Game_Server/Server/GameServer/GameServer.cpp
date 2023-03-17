fd4d27b6f45f5b64b0fa36ba6bae5d0f181c0c4b
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-03-17 15:40:33 +0900

    TLS - 각 쓰레드만의 저장 공간인 TLS를 알아봄

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -7,53 +7,30 @@
 #include <Windows.h>
 #include <future>
 
-atomic<bool> ready;
-int32 value;
+thread_local int32 LThreadId = 0;
+// int32 LThreadId = 0;
 
-void Producer()
+void ThreadMain(int threadId)
 {
-	value = 10;
+	LThreadId = threadId;
 
-	ready.store(true, memory_order::memory_order_seq_cst);
-}
-
-void Consumer()
-{
-	while (ready.load(memory_order::memory_order_seq_cst) == false)
-		;
-
-	cout << value << endl;
+	while (true)
+	{
+		cout << "HI! I am Thread " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
 }
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
+	vector<thread> threads;
 
-	// Memory Model (정책)
-	// 1) Sequentially Consistent (sep_cst)
-	// 2) Acquire_Release (consume, acquire, release, acq_rel)
-	// 3) Relaxed (relaxed)
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
 
-	// 1) seq_cst
-	// 가시성 문제 바로 해결! 코드 재배치 바로 해결!
-	// 
-	// 2) acquire-release
-	// 딱 중간!
-	// release 명령 이전의 메모리 명령들이 , 해당 명령 이후로 재배치 되는 것을 금지 
-	// 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면 
-	// release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능 (가시성 보장)
-	 
-	// 3) relaxed
-	// 너무나도 자유롭다!
-	// 코드 재배치도 멋대로 가능! 가시성 해결 NO!
-	// 가장 기본 조건 (동일 객체에 대한 동일 수정 순서만 보장)
-
-	// 추가로 인텔, AMD CPU의 경우에는 애당초 순차적 일관성을 보장해서
-	// sep_cst를 써도 별다른 차이가 없습니다.그래서 그냥 냅둬도 된다는겁니다.
-	// ARM 같은 경우에는 꽤 차이가 있어서 의미가 있다고 합니다.
+	for (thread& t : threads)
+		t.join();
 }
