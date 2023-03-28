9621c956fa8567c20a73203700a3759f6c2c4756
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-03-29 01:09:58 +0900

    Lock Free Queue - 락 프리 큐 구현 실습

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -1,25 +1,23 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-#include <thread>
 #include <atomic>
 #include <mutex>
-#include <Windows.h>
+#include <windows.h>
 #include <future>
 #include "ConcurrentQueue.h"
 #include "ConcurrentStack.h"
 
-
-LockQueue<int32> q;
+LockFreeQueue<int32> q;
 LockFreeStack<int32> s;
 
 void Push()
 {
 	while (true)
-	{ 
+	{
 		int32 value = rand() % 100;
-		s.Push(value);
-		
+		q.Push(value);
+
 		this_thread::sleep_for(1ms);
 	}
 }
@@ -28,22 +26,20 @@ void Pop()
 {
 	while (true)
 	{
-		auto data = s.TryPop();
+		auto data = q.TryPop();
 		if (data != nullptr)
 			cout << (*data) << endl;
 	}
 }
 
+
 int main()
 {
-	shared_ptr < int32> ptr;
-	bool result = std::atomic_is_lock_free(&ptr);
-
 	thread t1(Push);
 	thread t2(Pop);
 	thread t3(Pop);
 
 	t1.join();
 	t2.join();
-	t2.join();
+	t3.join();
 }
