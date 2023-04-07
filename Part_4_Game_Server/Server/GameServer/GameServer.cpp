334d8ae53459b14421f91cf552a1cc1dbc310823
Seonguk Cheon <cjstjddnr12@gmail.com>
2023-04-07 17:35:46 +0900

    멀티 쓰레드 연습문제 - 범위내 소수 개수 카운팅

--- Part_4_Game_Server/Server/GameServer/GameServer.cpp
+++ Part_4_Game_Server/Server/GameServer/GameServer.cpp
@@ -7,31 +7,65 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
+// 소수 구하기 
 
+/*
+	1과 자기 자신으로만 나누어 떨어지는 1보다 큰 양의 정수라고 합니다. 
+
+*/
+
+atomic<int> result = 0;
+
+bool isPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if (!(number % i))
+			return false;
+	}
+
+	return true;
+}
+
+void ThreadCount(int start, int end)
+{
+	for (int i = start; i <= end; i++)
+	{
+		if (!(isPrime(i)))
+			continue;
+
+		result++;
+	}
+}
 
 int main()
 {
-	GThreadManager->Launch([=]()
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	const int MAX_NUMBER = 100'0000;
 
-	GThreadManager->Launch([=]()
-		{
-			while (true)
-			{
-				cout << "AccountThenAccount" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	// 1 ~ MAX_NUMBER까지의 소수 개수 
 
-	GThreadManager->Join();
+	int coreCount = thread::hardware_concurrency();
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	vector<thread> threads = vector<thread>(coreCount);
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+		threads[i] = thread([start, end]()
+			{
+				ThreadCount(start, end);
+			});
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << result << endl;
 }
